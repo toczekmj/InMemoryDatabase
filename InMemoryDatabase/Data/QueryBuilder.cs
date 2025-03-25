48c0db0294d666c5c93b1c4d6001729e81da37e4
Michał Toczek <toczekmj@gmail.com>
2025-03-25 20:44:10 +0100

    feat: implement QueryBuilder.cs (#3)

--- InMemoryDatabase/Data/QueryBuilder.cs
+++ InMemoryDatabase/Data/QueryBuilder.cs
@@ -1,56 +1,89 @@
-﻿using InMemoryDatabase.Public_Interfaces;
+﻿using InMemoryDatabase.Internal_Interfaces;
+using InMemoryDatabase.Public_Interfaces;
 
 namespace InMemoryDatabase.Data;
 
 public class QueryBuilder : IQueryBuilder
 {
+    private readonly ITableInternal? _table;
+    private readonly string[]? _columns;
+    private string? _orderBy;
+    private string? _groupBy;
+    private bool _descending;
+    private int? _limit;
+    private Func<dynamic, bool>? _wherePredicate;
+
+    public QueryBuilder(ITable? table, string[] columns)
+    {
+        _table = table as ITableInternal;
+        _columns = columns;
+    }
+    
     public IQueryBuilder Where(Func<dynamic, bool> predicate)
     {
-        throw new NotImplementedException();
+        _wherePredicate = predicate;
+        return this;
     }
 
     public IQueryBuilder OrderBy(string column, bool descending = false)
     {
-        throw new NotImplementedException();
+        _orderBy = column;
+        _descending = descending;
+        return this;
     }
 
     public IQueryBuilder GroupBy(string column)
     {
-        throw new NotImplementedException();
+        _groupBy = column;
+        return this;
     }
 
     public IQueryBuilder Limit(int count)
     {
-        throw new NotImplementedException();
+        _limit = count;
+        return this;
     }
 
-    public int Count()
+    public int? Count() => Execute()?.Count; 
+
+    public double? Sum(string column) => CalculateDouble(column, rows => rows.Sum());
+
+    public double? Avg(string column) => CalculateDouble(column, rows => rows.Average());
+
+    public object? Min(string column) => CalculateObject(column, rows => rows.Min());
+
+    public object? Max(string column) => CalculateObject(column, rows => rows.Max());
+
+    public List<dynamic>? Execute()
     {
-        throw new NotImplementedException();
+        return _table?.ExecuteQuery(_wherePredicate, _columns, _orderBy, _groupBy, _descending, _limit);
+    }
+    
+    #region private methods
+    private double? CalculateDouble(string column, Func<IEnumerable<double>, double> aggregation)
+    {
+        IEnumerable<double>? rows = Execute()?.Select(row =>
+        {
+            if (row is not Dictionary<string, object> rowDict)
+                throw new ArgumentException(
+                    $"Row is not a {typeof(Dictionary<string, object>)} please check your database configuration.");
+            return Convert.ToDouble(rowDict[column]);
+        });
+        
+        return rows is null ? null : aggregation(rows);
     }
 
-    public double Sum(string column)
+    private object? CalculateObject(string column, Func<IEnumerable<dynamic>, object?> aggregation)
     {
-        throw new NotImplementedException();
+        IEnumerable<dynamic>? rows = Execute()?.Select(row =>
+        {
+            if (row is not Dictionary<string, object> rowDict)
+                throw new ArgumentException(
+                    $"Row is not a {typeof(Dictionary<string, object>)} please check your database configuration.");
+            return rowDict[column];
+        });
+        
+        return rows is null ? null : aggregation(rows);
     }
-
-    public double Avg(string column)
-    {
-        throw new NotImplementedException();
-    }
-
-    public object? Min(string column)
-    {
-        throw new NotImplementedException();
-    }
-
-    public object? Max(string column)
-    {
-        throw new NotImplementedException();
-    }
-
-    public List<dynamic> Execute()
-    {
-        throw new NotImplementedException();
-    }
+    #endregion
 }
--- InMemoryDatabase/Public Interfaces/IQueryBuilder.cs
+++ InMemoryDatabase/Public Interfaces/IQueryBuilder.cs
@@ -6,10 +6,12 @@ public interface IQueryBuilder
     IQueryBuilder OrderBy(string column, bool descending = false);
     IQueryBuilder GroupBy(string column);
     IQueryBuilder Limit(int count);
-    int Count();
-    double Sum(string column);
-    double Avg(string column);
+    int? Count();
+    double? Sum(string column);
+    double? Avg(string column);
     object? Min(string column);
     object? Max(string column);
-    List<dynamic> Execute();
+    List<dynamic>? Execute();
+    // TODO: Add Join method
+    // TODO: Add Update method
 }
